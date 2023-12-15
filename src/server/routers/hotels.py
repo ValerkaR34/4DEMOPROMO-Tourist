16a6e70297d91532d3fae067ca762f3dde5eee9a
ValerkaR34 <volmar2005@mail.ru>
2023-12-15 19:11:13 +0300

    исправил чутка, надо бы темп работы увеличить, я так ничего не добьюсь..

--- src/server/routers/hotels.py
+++ src/server/routers/hotels.py
@@ -1,33 +1,36 @@
 from typing import List
 
-from  fastapi import APIRouter
+import fastapi
 
-router = APIRouter()
+hotels_router = fastapi.APIRouter(prefix="/hotels", tags=["Hotels"])
 
 from ..models import  Hotels
 
 from server.resolvers.hotel import  new_hotel,get_hotel,get_all_hotel,delete_hotel,update_hotel
 
+@hotels_router.get("/")
+def start_page():
+    return ""
 
-@router.get('/')
+@hotels_router.get('/get/')
 def get_hotels() -> List[Hotels]:
     return get_all_hotel()
 
 
-@router.get('/{HotelID}')
+@hotels_router.get('/get/{HotelID}')
 def get_currenr_hotel(HotelID: int) -> Hotels:
     return get_hotel(HotelID)
 
-@router.post('/')
+@hotels_router.post('/new/')
 def add_hotel(new_hotel: Hotels) -> Hotels:
     return new_hotel(Hotels)
 
 
-@router.put('/{HotelID}')
+@hotels_router.put('/update/{HotelID}')
 def updates_hotel(HotelID: int, updated_hotel: Hotels) -> Hotels:
     return update_hotel(Hotels=updated_hotel, HotelID=HotelID)
 
 
-@router.delete("/{HotelID}")
+@hotels_router.delete("/delete/{HotelID}")
 def delete_hotels(HotelID: int) -> int:
     return delete_hotel(HotelID)
